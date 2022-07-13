9814ecf8d0151da3d6bc64933da721073ef76c62
ホー・ミン・ヒエウ <oppahd96@gmail.com>
2022-07-13 10:49:13 +0700

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import {Box} from '@chakra-ui/react'
 
 const Footer = () => (
     <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.500">
-        2022 MinHiu, Inc.
+        2022 Real Estate, Inc.
     </Box>
 )
 export default Footer;
